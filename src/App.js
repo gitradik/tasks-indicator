f3ecd97f67cf66b3b4ba0e87e8ccc4e1ec30692f
gitradik <rminzp17@gmail.com>
2019-10-30 13:51:28 +0200

    tasks-indicator

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             <IndicatorTest
                 from={counter}
                 to={to}
-                radius={70}
+                radius={150}
             />
             <button
                 style={{ width: '50px', height: '25px', cursor: 'pointer' }}
